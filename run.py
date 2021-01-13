4267f16f29aa99277d123ab4ee75abc5ccfaba1d
jgcook <34443196+jgcook@users.noreply.github.com>
2021-01-13 14:50:33 +0000

    Changing a few comments

--- run.py
+++ run.py
@@ -10,6 +10,9 @@ from portfolio_generator import EqualWeightedPortfolioGenerator
 from backtesting import WeightedPortfolioBacktester
 
 if __name__ == '__main__':
+    # filepath to price data must be specified in command line
+    filepath = sys.argv[1]
+    
     # Create signal generator
     config = SignalGeneratorConfig(rolling_avg_days=252)
     signal_generator = MomentumSignalGenerator(config)
@@ -18,7 +21,6 @@ if __name__ == '__main__':
     portfolio_generator = EqualWeightedPortfolioGenerator(signal_generator)
     
     # Load price data into dataframe
-    filepath = sys.argv[1]
     dataset = ExampleCSVDataset(filepath)
     prices_df = dataset.read()
     
